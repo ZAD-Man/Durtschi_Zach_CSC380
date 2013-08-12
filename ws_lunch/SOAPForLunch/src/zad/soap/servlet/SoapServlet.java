c5cc4a11e85359f97dbcd7dacb32eb1d58227f6c
Zach Durtschi <The.ZAD.One@GMail.com>
2013-08-12 00:52:03 -0600

    Adding .gitignore, a few things for soapLunch too
    It seems I can't get it to ignore files that have already been
    committed, but I hope this will work for future commits.

--- ws_lunch/SOAPForLunch/src/zad/soap/servlet/SoapServlet.java
+++ ws_lunch/SOAPForLunch/src/zad/soap/servlet/SoapServlet.java
@@ -5,12 +5,25 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
 import java.io.IOException;
+import java.io.InputStream;
 
 @WebServlet("/SoapServlet")
 public class SoapServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            JAXBContext jaxbContext = JAXBContext.newInstance("response");
+            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+            InputStream inputStream = request.getInputStream();
 
+            unmarshaller.unmarshal(inputStream);
+
+        } catch (JAXBException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
     }
 
 }
